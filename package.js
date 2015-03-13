05d3b58588e346d1d7ab25d559ebf788a1d52869
Gwen <gwendall.esnault@gmail.com>
2015-03-13 02:44:52 +0100

    added reactive-var dep

--- package.js
+++ package.js
@@ -2,13 +2,14 @@ Package.describe({
   name: "gwendall:templates-watch",
   summary: "Watch changes of specific template variables",
   git: "https://github.com/gwendall/meteor-templates-watch.git",
-  version: "0.1.1"
+  version: "0.1.2"
 });
 
 Package.onUse(function (api, where) {
 
   api.use([
-    "blaze@2.0.4"
+    "blaze@2.0.4",
+    "reactive-var@1.0.4"
   ], "client");
 
   api.addFiles([
